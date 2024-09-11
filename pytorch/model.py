0e35567649fc9f958a1a6f7c994393e5688f680d
satojkovic <satojkovic@gmail.com>
2024-09-11 23:32:07 +0900

    Add model.py

---
+++ pytorch/model.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import math
+import numpy as np
+import time
+from dataclasses import dataclass
+from entering import Optional, Tuple, List
+import pandas as pd
+import matplotlib.pyplot as plt
