0f5346d31164024508af9bf32f77e409ef8d7687
satojkovic <satojkovic@gmail.com>
2024-09-20 00:02:52 +0900

    Add skeleton

--- pytorch/model.py
+++ pytorch/model.py
@@ -9,3 +9,8 @@ from dataclasses import dataclass
 from entering import Optional, Tuple, List
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+class Llama3(nn.Module):
+    def __init__(self, params, hparams):
+        pass
